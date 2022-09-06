d9b678956e93e8d5f0b42260b2bef5543df49290
ck <cilerk@icloud.com>
2022-09-06 12:44:40 +0300

    deneme

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ck0509/testproject
+
+go 1.18
--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/ck0509/testproject/utils"
+)
 
 func main() {
 	fmt.Println("hello ck")
+
+	result := utils.Sum(10, 20)
+
+	fmt.Println(result)
+
 }
